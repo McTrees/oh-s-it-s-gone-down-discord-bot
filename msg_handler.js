de76070b2c083b46d34a02d4380ce2044521a3c2
Oliver Hynds <oliver.hynds@gmail.com>
2018-05-13 15:21:11 +0100

    aded msg stuff

--- msg_handler.js
+++ msg_handler.js
@@ -6,7 +6,32 @@ module.exports = function(msg, client) {
   if (msg.author == client.user) {return}; //ignore own messages
   if (msg.guild === null){msg.reply("currently you cannot send commands in a DM");return};//prevent DMs
 
-  if (msg.content[0] == config.bot_prefix){ //if bot command
-    
+  if (msg.content[0] == config.bot_prefix){ //if bot commands
+    msg.content = msg.content.substring(1); //remove bot prefix
+
+    if (msg.content[0] = "e"){ //enrole comand
+
+    }
+
+    if (msg.content[0] = "e"){ //list comand
+
+    }
+
+    if (msg.content[0] = "s"){ //status comand
+
+    }
+
+    if (msg.content[0] = "t"){ //test comand
+
+    }
+
+    if (msg.content[0] = "h"){ //help comand
+
+    }
+
+    else {
+      msg.reply("Sorry, that is not a valid comand. run ```!help``` for more info.");
+    }
+
   }
 }
